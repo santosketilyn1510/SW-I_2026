ec1757872d22a88f55ed692775ad99c504d50856
ketilyn santos <santosketilyn1510@gmail.com>
2026-02-27 18:54:26 -0300

    Add example05.php with PHP and HTML structure

---
+++ aula02/exemplo05.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 02</title>
+</head>
+<body>
+    <?php
+    $a = 4;
+    $b = "101 dalmatas";
+
+    $s = $a + $b;
+
+    echo $s;
+    ?>
+</body>
+</html>
