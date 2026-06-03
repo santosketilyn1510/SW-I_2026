d9c37106be1b59e362a227d0b9b2174a8f88513d
ketilyn santos <santosketilyn1510@gmail.com>
2026-06-03 08:29:03 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,25 +1,46 @@
 <?php
-$msg = "sua média é:";
-$nome = "ketilyn";
-$m1 = 8;
-$m2 = 9;
-$m3 = 1;
-$média = 0;
-$média = (($m1 + $m2 + $m3)/3);
 
-if ($média >=9) {
-    echo "MB";
+$cpf = preg_replace('/[^0-9]/', '', $_POST['cpf']);
+
+if (strlen($cpf) != 11) {
+    echo "<h2>$nome, CPF inválido!</h2>";
+    exit;
 }
 
-if ($média < 8 && $média >= 7) {
-    echo "B";
+if (preg_match('/(\d)\1{10}/', $cpf)) {
+    echo "<h2>$nome, CPF inválido!</h2>";
+    exit;
 }
 
-if ($média < 7 && $média >= 4) {
-echo "R";
+$soma = 0;
+for ($i = 0, $peso = 10; $i < 9; $i++, $peso--) {
+    $soma += $cpf[$i] * $peso;
 }
 
-if ($média < 4) {
- echo "I";
+$resto = ($soma * 10) % 11;
+if ($resto == 10) {
+    $resto = 0;
 }
-   
+
+if ($resto != $cpf[9]) {
+    echo "<h2>$nome, CPF inválido!</h2>";
+    exit;
+}
+
+$soma = 0;
+for ($i = 0, $peso = 11; $i < 10; $i++, $peso--) {
+    $soma += $cpf[$i] * $peso;
+}
+
+$resto = ($soma * 10) % 11;
+if ($resto == 10) {
+    $resto = 0;
+}
+
+if ($resto != $cpf[10]) {
+    echo "<h2>$nome, CPF inválido!</h2>";
+} else {
+    echo "<h2>$nome, CPF válido!</h2>";
+}
+
+?>
